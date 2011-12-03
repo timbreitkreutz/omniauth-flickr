2a7e24ae7d2aa8ed08a0f1689e3788b97a0b12af
Tim Breitkreutz <tim@sbrew.com>
2011-12-02 20:47:15 -0700

    Add rest of user info to info tag

--- lib/omniauth/strategies/flickr.rb
+++ lib/omniauth/strategies/flickr.rb
@@ -4,7 +4,9 @@ require 'multi_json'
 module OmniAuth
   module Strategies
     class Flickr < OmniAuth::Strategies::OAuth
+      
       option :name, 'flickr'
+      
       option :client_options, {
         :access_token_path => "/services/oauth/access_token",
         :authorize_path => "/services/oauth/authorize",
@@ -17,14 +19,24 @@ module OmniAuth
       }
       
       info do 
-        {
-          :username => access_token.params['username'],
-          :full_name => access_token.params['fullname']
-        }
+        user_info
       end
       
       extra do
         {}
+      end
+      
+      def user_info
+        if @user_info.blank?
+          @user_info = {}
+          # This is a public API and does not need signing or authentication
+          url = "/services/rest/?api_key=#{options.consumer_key}&format=json&method=flickr.people.getInfo&nojsoncallback=1&user_id=#{uid}"
+          response = Net::HTTP.get(options.client_options[:site].gsub(/.*:\/\//, ""), url)
+          @user_info ||= MultiJson.decode(response.body) if response
+        end
+        @user_info
+      rescue ::Errno::ETIMEDOUT
+        raise ::Timeout::Error
       end
     end
   end
